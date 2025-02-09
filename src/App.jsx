34f0c7cac131da3daa9fd2580d93829be29bf0f8
Rushikesh Sonar <rusbharat@deloitte.com>
2025-02-09 12:56:05 +0530

    added code for deploying

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,13 @@ export default class App extends Component {
       <Router>
       <Navbar/>
         <Routes>
-          <Route exact path="/" element={<News key="general" category="general" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>
-          <Route exact path="/general" element={<News key="general" category="general" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>
-          <Route exact path="/sports" element={<News  key="sports" category="sports" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>
-          <Route exact path="/business" element={<News key="business" category="business" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>  
-          <Route exact path="/technology" element={<News key="technology" category="technology" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>
-          <Route exact path="/health" element={<News key="health" category="health" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>
-          <Route exact path="/entertainment" element={<News key="entertainment" category="entertainment" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>
+          <Route exact path="/NewsCurrent" element={<News key="general" category="general" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>
+          <Route exact path="/NewsCurrent/general" element={<News key="general" category="general" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>
+          <Route exact path="/NewsCurrent/sports" element={<News  key="sports" category="sports" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>
+          <Route exact path="/NewsCurrent/business" element={<News key="business" category="business" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>  
+          <Route exact path="/NewsCurrent/technology" element={<News key="technology" category="technology" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>
+          <Route exact path="/NewsCurrent/health" element={<News key="health" category="health" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>
+          <Route exact path="/NewsCurrent/entertainment" element={<News key="entertainment" category="entertainment" api_key="500bdbc1b2d147c2b4f352b3358d546c"/>}></Route>
         </Routes>
       </Router>
       </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,29 +9,29 @@ export class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/general">NewsCurrent</Link>
+                    <Link className="navbar-brand" to="/NewsCurrent">NewsCurrent</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                        <Link className="nav-link" aria-current="page" to="/general">General</Link>
+                        <Link className="nav-link" aria-current="page" to="/NewsCurrent/general">General</Link>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" to="/sports">Sports</Link>
+                        <Link className="nav-link" to="/NewsCurrent/sports">Sports</Link>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" aria-current="page" to="/business">Business</Link>
+                        <Link className="nav-link" aria-current="page" to="/NewsCurrent/business">Business</Link>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" aria-current="page" to="/technology">Technology</Link>
+                        <Link className="nav-link" aria-current="page" to="/NewsCurrent/technology">Technology</Link>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" aria-current="page" to="/health">Health</Link>
+                        <Link className="nav-link" aria-current="page" to="/NewsCurrent/health">Health</Link>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" aria-current="page" to="/entertainment">Entertainment</Link>
+                        <Link className="nav-link" aria-current="page" to="/NewsCurrent/entertainment">Entertainment</Link>
                         </li>
                     </ul>
                     </div>

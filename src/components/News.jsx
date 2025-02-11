5f1ac429b9498fe4f20e1ea99a590fa34d43e4eb
Rushikesh Sonar <rusbharat@deloitte.com>
2025-02-11 12:08:52 +0530

    added infinite scroll and removed next prev buttons

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,30 +1,30 @@
 import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 import Spinner from './Spinner';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 export class News extends Component {
   
   constructor(){
     super();
-    this.maxPage=0;
     this.pageSize=15;
     this.state={
       news_articles : [],
       loading: false,
       pageNumber: 1,
+      totalResults:0,
     }
     
   }
-  FetchNewsApi = async (page_number)=>{
+  FetchNewsApi = async ()=>{
     this.setState({loading: true})
     document.title = `NewsCurrent - ${((this.props.category).charAt(0)).toUpperCase()+(this.props.category).slice(1)}`
     try{
-      let response = await fetch(`https://newsapi.org/v2/top-headlines?category=${this.props.category}&pageSize=${this.pageSize}&page=${page_number}&apiKey=${this.props.api_key}`)
+      let response = await fetch(`https://newsapi.org/v2/top-headlines?category=${this.props.category}&pageSize=${this.pageSize}&page=${this.state.pageNumber}&apiKey=${this.props.api_key}`)
       if(response.status===200){
         let data = await response.json()
-        this.setState({news_articles: data.articles,loading: false})
-        return data
+        this.setState({news_articles: data.articles,loading: false, totalResults: data.totalResults})
       }
       else{
           this.setState({news_articles: ["Backend is down"]})
@@ -35,17 +35,30 @@ export class News extends Component {
     }
     }
 
-  async componentDidMount(){
-    let data = await this.FetchNewsApi(1)
-    this.maxPage = Math.ceil(data?data.totalResults/this.pageSize:0)
+  componentDidMount(){
+    this.FetchNewsApi()
   }
-  next = async ()=>{
-    this.FetchNewsApi(this.state.pageNumber+1)
-    this.setState({pageNumber: this.state.pageNumber+1})
+  fetchMoreData = async ()=>{
+    this.setState({loading: true})
+    document.title = `NewsCurrent - ${((this.props.category).charAt(0)).toUpperCase()+(this.props.category).slice(1)}`
+    try{
+      let response = await fetch(`https://newsapi.org/v2/top-headlines?category=${this.props.category}&pageSize=${this.pageSize}&page=${this.state.pageNumber+1}&apiKey=${this.props.api_key}`)
+      if(response.status===200){
+        let data = await response.json()
+        this.setState({news_articles: this.state.news_articles.concat(data.articles),pageNumber: this.state.pageNumber+1,loading: false})
+      }
+      else{
+        this.setState({loading:false})
+        let info = document.createElement("h5")
+        info.innerText = "Cant refresh data! Backend down"
+        info.setAttribute("class","text-center")
+        let row = document.querySelector("#newselementrow")
+        row.appendChild(info)
+      }
     }
-  prev = async ()=>{
-    this.FetchNewsApi(this.state.pageNumber-1)
-    this.setState({pageNumber: this.state.pageNumber-1})
+    catch(error){
+      console.log("error from fetchNewsApi",error)
+    }
     }
 
   render() {
@@ -55,20 +68,25 @@ export class News extends Component {
         <div className='loader'>
         {this.state.loading && <Spinner/>}
         </div>
-        <div className="row">
-          {this.state.news_articles.map(element=>{
+        <InfiniteScroll
+          dataLength={this.state.news_articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.news_articles.length<this.state.totalResults}
+          loader={this.state.loading && <Spinner/>}
+        >
+        <div className="container">
+        <div className="row" id="newselementrow">
+          {this.state.news_articles.map((element,index)=>{
             if(element==="Backend is down"){
               return <h1 key="serverDown">Backend is down</h1>
             }
-            return !this.state.loading && <div className="col-md-4" key={element.url}>
+            return <div className="col-md-4" key={index+element.url}>
             <Newsitem title={element.title?element.title:""} description={element.description?element.description:""} img_url={element.urlToImage} news_url={element.url} date={element.publishedAt?element.publishedAt:"00:00:00"} author={element.author?element.author:"unknown"} source = {element.source.name?element.source.name:"unknown"}/>
             </div>
           })}
         </div>
-        <div className="btn-group mb-2" role="group" >
-          <button type="button" className="btn btn-primary" disabled={this.state.pageNumber>1?false:true} onClick={this.prev}><i className="fa-solid fa-arrow-left"></i> Prev</button>
-          <button type="button" className="btn btn-primary mx-2" disabled={this.state.pageNumber<this.maxPage?false:true} onClick={this.next}>Next <i className="fa-solid fa-arrow-right"></i></button>
         </div>
+        </InfiniteScroll>
       </div>
     )
   }
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import loader from '../assets/loading.gif'
 export class Spinner extends Component {
   render() {
     return (
-      <div>
+      <div className='text-center my-3'>
         <img src={loader} alt='loading'/>
       </div>
     )
